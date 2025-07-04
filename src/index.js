84aa82d2d3655338c134e556b57514396bc88fce
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-07-04 08:24:34 +0530

    all done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import 'dotenv/config';
 import chat from '../routes/chat/chat.js';
+import cors from 'cors';
 import loaders from '../routes/loaders/loaders.js';
 
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
 app.use('/loaders', loaders.router);
 app.use('/ai', chat.router);
 app.get('/', (req, res) => {
